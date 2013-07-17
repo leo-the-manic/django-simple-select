0ccbc1d8d696759fe75e4062c528cce9995486d7
leo-the-manic <manicleo@gmail.com>
2013-07-17 14:55:25 -0400

    Use a JSON service to populate suggestions.

--- simpleselect/tests.py
+++ simpleselect/tests.py
@@ -1,16 +1,26 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
+import unittest
 
-Replace this with more appropriate tests for your application.
-"""
+import mock
 
-from django.test import TestCase
+from . import views
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class JSONResponseTest(unittest.TestCase):
+    """Tests for the JSONResponse class."""
+
+    def test_sets_content_type(self):
+        """JSON responses have a header 'content-type: application/json'"""
+        resp = views.JSONResponse()
+        self.assertEquals(resp['content-type'], 'application/json')
+
+    def test_converts_to_json(self):
+        """JSON responses convert their content to JSON."""
+        json = mock.MagicMock(return_value='bar')
+        resp = views.JSONResponse(content='foo', json_service=json)
+        json.assert_called_with('foo')
+        self.assertEquals(resp.content, 'bar')
+
+        json = mock.MagicMock(return_value='buzz')
+        resp = views.JSONResponse(content='fizz', json_service=json)
+        json.assert_called_with('fizz')
+        self.assertEquals(resp.content, 'buzz')
