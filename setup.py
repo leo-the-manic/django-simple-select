94275f42485a6b33d4322f9cddb9ec833a2e343e
leo-the-manic <manicleo@gmail.com>
2013-07-19 15:06:35 -0400

    Correctly include JS file in package

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name="django-simple-select",
     packages=['simpleselect'],
-    version='0.1',
+    version='0.1.2',
     description='An autocompleting select widget for Django',
     author='leo-the-manic',
     author_email='manicleo@gmail.com',
