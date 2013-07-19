c15f0548a8da0ba364091bcae9d45be3e1c16091
leo-the-manic <manicleo@gmail.com>
2013-07-19 16:14:18 -0400

    More packaging issues (seems to install now)

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 from distutils.core import setup
 
+# copy README.rst to README; Github needs .rst, PyPI needs bare
+import sys, os, shutil
+if 'sdist' in sys.argv:
+    readme = os.path.join(os.path.dirname(__file__), "README.rst")
+    shutil.copyfile(readme, os.path.splitext(readme)[0])
+
 setup(
     name="django-simple-select",
-    packages=['simpleselect'],
-    version='0.1.2',
+    version='0.1.8',
     description='An autocompleting select widget for Django',
     author='leo-the-manic',
     author_email='manicleo@gmail.com',
     url='https://github.com/leo-the-manic/django-simple-select/',
+
+    packages=['simpleselect'],
+    package_data={'simpleselect': ['static/*']},
 )
