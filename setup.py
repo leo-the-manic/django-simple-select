16d8dde05e94b00b0e001c3a3064f4856bd7e184
leo-the-manic <manicleo@gmail.com>
2013-07-19 10:28:44 -0400

    Add distutils stuff

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="django-simple-select",
+    packages=['simpleselect'],
+    version='0.1',
+    description='An autocompleting select widget for Django',
+    author='leo-the-manic',
+    author_email='manicleo@gmail.com',
+    url='https://github.com/leo-the-manic/django-simple-select/',
+)
