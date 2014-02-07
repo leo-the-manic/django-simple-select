d006f4a9649ee09ead180f8e4ff270d81ad0b56c
leo-the-manic <manicleo@gmail.com>
2014-02-07 17:03:56 -0500

    __unicode__ -> __str__

--- testproject/demo/models.py
+++ testproject/demo/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Company(models.Model):
     name = models.CharField(max_length=50)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.name
 
 
@@ -13,6 +13,6 @@ class Person(models.Model):
     last_name = models.CharField(max_length=50)
     company = models.ForeignKey(Company)
 
-    def __unicode__(self):
+    def __str__(self):
         return "{} {} ({})".format(self.first_name, self.last_name,
                                    self.company)
